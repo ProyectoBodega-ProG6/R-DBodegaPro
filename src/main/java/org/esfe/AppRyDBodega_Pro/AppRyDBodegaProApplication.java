9183a6f07275fc6ca71b348b9245c95beebe75ef
ANDREA-ROCIO-PEREZ-ARRIOLA <pa25001@esfe.agape.edu.sv>
2025-07-28 19:41:44 -0600

    Se inicializo el proyecto

---
+++ src/main/java/org/esfe/AppRyDBodega_Pro/AppRyDBodegaProApplication.java
@@ -0,0 +1,13 @@
+package org.esfe.AppRyDBodega_Pro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppRyDBodegaProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppRyDBodegaProApplication.class, args);
+	}
+
+}
