bcdefad00936a195d212df562d573d98eaa6e57e
ANDREA-ROCIO-PEREZ-ARRIOLA <pa25001@esfe.agape.edu.sv>
2025-07-29 07:42:00 -0600

    Se creo la clase usuarios.

---
+++ src/main/java/org/esfe/AppRyDBodega_Pro/modelos/Usuario.java
@@ -0,0 +1,10 @@
+package org.esfe.AppRyDBodega_Pro.modelos;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+
+}
