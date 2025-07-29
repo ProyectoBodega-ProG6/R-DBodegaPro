6b652d9b5ffab7071cd86163353d690ea3b5afab
ANDREA-ROCIO-PEREZ-ARRIOLA <pa25001@esfe.agape.edu.sv>
2025-07-28 22:34:59 -0600

    Se descargo una dependecia en el archivo pom y se edito la clase Roles.

--- src/main/java/org/esfe/AppRyDBodega_Pro/modelos/Roles.java
+++ src/main/java/org/esfe/AppRyDBodega_Pro/modelos/Roles.java
@@ -1,5 +1,48 @@
 package org.esfe.AppRyDBodega_Pro.modelos;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "roles")
 public class Roles {
 
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotBlank(message = "El nombre del rol es requerido")
+    private String nombre;
+
+    @NotBlank(message = "La descripción del rol es requerida")
+    private String descripcion;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    
 }
