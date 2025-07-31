0524df9e100e94fe8954554a60682123ccb8469f
DANIEL-STEVEN-L0PEZ-HERNANDEZ <lh25001@esfe.agape.edu.sv>
2025-07-31 15:21:26 -0600

    Se creo el repositorio de Rol

---
+++ src/main/java/org/esfe/AppRyDBodega_Pro/repositorios/IRolRepository.java
@@ -0,0 +1,7 @@
+package org.esfe.AppRyDBodega_Pro.repositorios;
+
+import org.esfe.AppRyDBodega_Pro.modelos.Rol;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IRolRepository extends JpaRepository<Rol, Integer> {
+}
