2f3d2835d53dd4b832ba8824355d737ce6a550f7
ANDREA-ROCIO-PEREZ-ARRIOLA <pa25001@esfe.agape.edu.sv>
2025-07-30 23:43:46 -0600

    Se creo la clase TipoMovimiento

---
+++ src/main/java/org/esfe/AppRyDBodega_Pro/modelos/TipoMovimiento.java
@@ -0,0 +1,10 @@
+package org.esfe.AppRyDBodega_Pro.modelos;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+
+@Entity
+@Table(name = "tipoMovimiento")
+public class TipoMovimiento {
+}
