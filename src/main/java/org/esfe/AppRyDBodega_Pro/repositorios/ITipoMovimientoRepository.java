1fdd8641d4e99c99b21b6f26dc8df31a949eebc4
DANIEL-STEVEN-L0PEZ-HERNANDEZ <lh25001@esfe.agape.edu.sv>
2025-07-31 15:23:24 -0600

    Se creo el repositorio de TipoMovimiento

---
+++ src/main/java/org/esfe/AppRyDBodega_Pro/repositorios/ITipoMovimientoRepository.java
@@ -0,0 +1,7 @@
+package org.esfe.AppRyDBodega_Pro.repositorios;
+
+import org.esfe.AppRyDBodega_Pro.modelos.TipoMovimiento;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ITipoMovimientoRepository extends JpaRepository<TipoMovimiento, Integer> {
+}
