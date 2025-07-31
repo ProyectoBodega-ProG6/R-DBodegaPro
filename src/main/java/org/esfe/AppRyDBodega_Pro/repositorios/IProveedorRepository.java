6ae596732b56c746d4d06e60a308c60703b0e3a8
DANIEL-STEVEN-L0PEZ-HERNANDEZ <lh25001@esfe.agape.edu.sv>
2025-07-31 15:19:54 -0600

    Se creo el repositorio de Proveedor

---
+++ src/main/java/org/esfe/AppRyDBodega_Pro/repositorios/IProveedorRepository.java
@@ -0,0 +1,7 @@
+package org.esfe.AppRyDBodega_Pro.repositorios;
+
+import org.esfe.AppRyDBodega_Pro.modelos.Proveedor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IProveedorRepository extends JpaRepository<Proveedor, Integer> {
+}
