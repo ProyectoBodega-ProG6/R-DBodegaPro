1e1002e608c8c7b5a112749bebe3d4884e86afc1
DANIEL-STEVEN-L0PEZ-HERNANDEZ <lh25001@esfe.agape.edu.sv>
2025-08-08 21:41:20 -0600

    Se creo el repositorio de Producto y se modifico el ProductoService y IProductoService

--- src/main/java/org/esfe/AppRyDBodega_Pro/servicios/interfaces/IProductoService.java
+++ src/main/java/org/esfe/AppRyDBodega_Pro/servicios/interfaces/IProductoService.java
@@ -7,10 +7,5 @@ import org.springframework.data.domain.Pageable;
 import java.util.*;
 
 public interface IProductoService {
-    Page<Producto> buscarTodosPaginados(Pageable pageable);
-    List<Producto> obtenerTodos();
-    Optional<Producto> buscarPorId(Integer id);
-    Producto crearOEditttar(Producto producto);
-    void eliminarPorId(Integer id);
 
 }
