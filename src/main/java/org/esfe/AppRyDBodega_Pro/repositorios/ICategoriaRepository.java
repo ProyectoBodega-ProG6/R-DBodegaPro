51942240b12415cd595c3d72180c06c4ce574b25
ANDREA-ROCIO-PEREZ-ARRIOLA <pa25001@esfe.agape.edu.sv>
2025-08-09 21:47:41 -0600

    Se completo  el repositorio de categoria

--- src/main/java/org/esfe/AppRyDBodega_Pro/repositorios/ICategoriaRepository.java
+++ src/main/java/org/esfe/AppRyDBodega_Pro/repositorios/ICategoriaRepository.java
@@ -1,7 +1,16 @@
 package org.esfe.AppRyDBodega_Pro.repositorios;
 
 import org.esfe.AppRyDBodega_Pro.modelos.Categoria;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ICategoriaRepository extends JpaRepository<Categoria, Integer> {
+
+    List<Categoria> findByNombreIgnoreCase(String nombre);
+    Page<Categoria> findByNombreContainingIgnoreCase(String nombre, Pageable pageable);
+    List<Categoria> findAllByOrderByNombreAsc();
+    Page<Categoria> findAllByOrderByNNombreAsc();
 }
