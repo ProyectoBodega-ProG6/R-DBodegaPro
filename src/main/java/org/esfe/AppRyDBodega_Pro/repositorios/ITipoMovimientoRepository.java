623e99ee5d9ceb3cfe3726374e041ed7f59813a5
DANIEL-STEVEN-L0PEZ-HERNANDEZ <lh25001@esfe.agape.edu.sv>
2025-08-09 23:22:15 -0600

    Se creo la funcion de ITipoMovimeintoRepository

--- src/main/java/org/esfe/AppRyDBodega_Pro/repositorios/ITipoMovimientoRepository.java
+++ src/main/java/org/esfe/AppRyDBodega_Pro/repositorios/ITipoMovimientoRepository.java
@@ -1,7 +1,21 @@
 package org.esfe.AppRyDBodega_Pro.repositorios;
 
 import org.esfe.AppRyDBodega_Pro.modelos.TipoMovimiento;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ITipoMovimientoRepository extends JpaRepository<TipoMovimiento, Integer> {
+
+    List<TipoMovimiento> findAll(Sort sort);
+    List<TipoMovimiento> findAllByOrderByNombreAsc();
+
+    Page<TipoMovimiento> findAll(Pageable pageable);
+
+    Page<TipoMovimiento> findAllByOrderByNombreAsc(Pageable pageable);
+
+
 }
