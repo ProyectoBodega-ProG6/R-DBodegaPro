9a363c5a04d013848803821f90cd2d47971cf4c5
DANIEL-STEVEN-L0PEZ-HERNANDEZ <lh25001@esfe.agape.edu.sv>
2025-08-09 22:31:00 -0600

    Se creo la funcion IRolRepository

--- src/main/java/org/esfe/AppRyDBodega_Pro/repositorios/IRolRepository.java
+++ src/main/java/org/esfe/AppRyDBodega_Pro/repositorios/IRolRepository.java
@@ -1,7 +1,14 @@
 package org.esfe.AppRyDBodega_Pro.repositorios;
 
 import org.esfe.AppRyDBodega_Pro.modelos.Rol;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IRolRepository extends JpaRepository<Rol, Integer> {
+
+    Page<Rol> findAll(Pageable pageable);
+
+
+    Page<Rol> findAllByOrderByNombreRolAsc(Pageable  pageable);
 }
