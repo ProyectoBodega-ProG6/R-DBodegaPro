c979b81e322a537f747c01b9b80ecc11ca11ab04
ANDREA-ROCIO-PEREZ-ARRIOLA <pa25001@esfe.agape.edu.sv>
2025-08-09 23:40:31 -0600

    Se completo el repositorio de MovimientoEntradaSalida

--- src/main/java/org/esfe/AppRyDBodega_Pro/repositorios/IMovimientoEntradaSalidaRepository.java
+++ src/main/java/org/esfe/AppRyDBodega_Pro/repositorios/IMovimientoEntradaSalidaRepository.java
@@ -2,7 +2,15 @@ package org.esfe.AppRyDBodega_Pro.repositorios;
 
 import org.esfe.AppRyDBodega_Pro.modelos.MovimientoEntradaSalida;
 import org.springframework.boot.json.JacksonJsonParser;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IMovimientoEntradaSalidaRepository extends JpaRepository<MovimientoEntradaSalida, Integer> {
+
+    Page<MovimientoEntradaSalida> findByProductoNombreContainingIgnoreCaseOrderByFechaAsc(String nombre, Pageable pageable);
+    Page<MovimientoEntradaSalida> findByTipoMovimientoIdOrderByFechaAsc(Integer idTipoMovimiento, Pageable pageable);
+    Page<MovimientoEntradaSalida> findByProductoNombreContainingIgnoreCaseAndTipoMovimientoIdOrderByFechaAsc(String nombre, Integer id, Pageable pageable);
+    Page<MovimientoEntradaSalida> findAllByOrderByFechaAsc(Pageable pageable);
+    Page<MovimientoEntradaSalida> findByProductoIdOrderByFechaAsc(Integer id, Pageable pageable);
 }
