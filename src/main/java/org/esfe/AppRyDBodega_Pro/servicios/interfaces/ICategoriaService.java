93dc522c8544b188890e61696fabdd0dd1cea0e4
ANDREA-ROCIO-PEREZ-ARRIOLA <pa25001@esfe.agape.edu.sv>
2025-08-11 09:50:18 -0600

    Se modifico la interfaz de categoria

--- src/main/java/org/esfe/AppRyDBodega_Pro/servicios/interfaces/ICategoriaService.java
+++ src/main/java/org/esfe/AppRyDBodega_Pro/servicios/interfaces/ICategoriaService.java
@@ -1,4 +1,31 @@
 package org.esfe.AppRyDBodega_Pro.servicios.interfaces;
 
+import org.esfe.AppRyDBodega_Pro.modelos.Categoria;
+import org.esfe.AppRyDBodega_Pro.modelos.Usuario;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface ICategoriaService {
+
+    Page<Categoria> buscarTodosPaginados(Pageable pageable);
+
+    List<Categoria> obtenerTodos();
+
+    Optional<Categoria> buscarPorId(Integer id);
+
+    Categoria createOrEditOne(Categoria categoria);
+
+    void eliminarPorId(Integer id);
+
+    List<Categoria> findByNombreIgnoreCase(String nombre);
+
+    Page<Categoria> findByNombreContainingIgnoreCase(String nombre, Pageable pageable);
+
+    List<Categoria> findAllByOrderByNombreAsc();
+
+    Page<Categoria> findAllByOrderByNombreAsc(Pageable pageable);
+
 }
