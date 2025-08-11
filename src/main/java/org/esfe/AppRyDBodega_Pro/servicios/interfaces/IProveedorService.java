4adc3f1c27608b712fdc71d028bb48b70fac55cd
ANDREA-ROCIO-PEREZ-ARRIOLA <pa25001@esfe.agape.edu.sv>
2025-08-11 10:01:03 -0600

    Se modifico la interfaz de proveedores

--- src/main/java/org/esfe/AppRyDBodega_Pro/servicios/interfaces/IProveedorService.java
+++ src/main/java/org/esfe/AppRyDBodega_Pro/servicios/interfaces/IProveedorService.java
@@ -1,4 +1,31 @@
 package org.esfe.AppRyDBodega_Pro.servicios.interfaces;
 
+import org.esfe.AppRyDBodega_Pro.modelos.Categoria;
+import org.esfe.AppRyDBodega_Pro.modelos.Producto;
+import org.esfe.AppRyDBodega_Pro.modelos.Proveedor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface IProveedorService {
+
+    Page<Proveedor> buscarTodosPaginados(Pageable pageable);
+
+    List<Proveedor> obtenerTodos();
+
+    Optional<Proveedor> buscarPorId(Integer id);
+
+    Proveedor createOrEditOne(Proveedor proveedor);
+
+    void eliminarPorId(Integer id);
+
+    Page<Proveedor> findAllByOrderByNombreAsc(Pageable pageable);
+
+    Page<Proveedor> findByNombreContainingIgnoreCaseOrderByNombreAsc(String nombre, Pageable pageable);
+
+    Page<Proveedor> findByNombreEmpresaContainingIgnoreCaseOrderByNombreEmpresaAsc(String nombreEmpresa, Pageable pageable);
+
+
 }
