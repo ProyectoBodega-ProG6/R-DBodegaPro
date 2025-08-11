2f15a07a1427332281f1e40ea0b3ea681a798432
DANIEL-STEVEN-L0PEZ-HERNANDEZ <lh25001@esfe.agape.edu.sv>
2025-08-11 09:41:51 -0600

    Se modifico la Interfaz TipoMovimiento

--- src/main/java/org/esfe/AppRyDBodega_Pro/servicios/interfaces/ITipoMovimientoService.java
+++ src/main/java/org/esfe/AppRyDBodega_Pro/servicios/interfaces/ITipoMovimientoService.java
@@ -1,4 +1,23 @@
 package org.esfe.AppRyDBodega_Pro.servicios.interfaces;
 
+import org.esfe.AppRyDBodega_Pro.modelos.Producto;
+import org.esfe.AppRyDBodega_Pro.modelos.TipoMovimiento;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface ITipoMovimientoService {
+
+    Page<Producto> buscarTodosPaginados(Pageable pageable);
+    List<Producto>obtenerTodos();
+    Optional<Producto> buscarPorId(Integer id);
+    Producto createOrEditOne(Producto producto);
+    void eliminarPorId (Integer id);
+    List<TipoMovimiento> listarOrdenados(Sort sort);
+    List<TipoMovimiento> listarOrdenadosPorNombreAsc();
+    Page<TipoMovimiento> listarPaginados(Pageable pageable);
+    Page<TipoMovimiento> listarPaginadosPorNombreAsc(Pageable pageable);
 }
