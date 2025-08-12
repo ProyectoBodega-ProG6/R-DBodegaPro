375b55a206dbeade1293d9443b2dfa9850a91726
ANDREA-ROCIO-PEREZ-ARRIOLA <pa25001@esfe.agape.edu.sv>
2025-08-12 09:50:51 -0600

    Se modifico la interfaz de servicio de Proveedor

--- src/main/java/org/esfe/AppRyDBodega_Pro/servicios/interfaces/IProveedorService.java
+++ src/main/java/org/esfe/AppRyDBodega_Pro/servicios/interfaces/IProveedorService.java
@@ -21,4 +21,8 @@ public interface IProveedorService {
 
     void eliminarPorId(Integer id);
 
+    Page<Proveedor> obtenerTodos(Pageable pageable);
+
+    Page<Proveedor> buscarPorNombreYEmpresa(String nombre, String nombreEmpresa, Pageable pageable);
+
 }
