ece65ab0039c4fbfa346c1f0b7eb7683092fe802
DANIEL-STEVEN-L0PEZ-HERNANDEZ <lh25001@esfe.agape.edu.sv>
2025-08-12 09:12:45 -0600

    Se realizo la interfaz de MovimientoEntradaSalida

--- src/main/java/org/esfe/AppRyDBodega_Pro/servicios/interfaces/IMovimientoEntradaSalidaService.java
+++ src/main/java/org/esfe/AppRyDBodega_Pro/servicios/interfaces/IMovimientoEntradaSalidaService.java
@@ -1,4 +1,21 @@
 package org.esfe.AppRyDBodega_Pro.servicios.interfaces;
 
+import org.esfe.AppRyDBodega_Pro.modelos.Categoria;
+import org.esfe.AppRyDBodega_Pro.modelos.MovimientoEntradaSalida;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface IMovimientoEntradaSalidaService {
+    Page<MovimientoEntradaSalida> buscarTodosPaginados(Pageable pageable);
+
+    List<MovimientoEntradaSalida> obtenerTodos();
+
+    Optional<MovimientoEntradaSalida> buscarPorId(Integer id);
+
+    MovimientoEntradaSalida createOrEditOne(MovimientoEntradaSalida movimientoEntradaSalida);
+
+    void eliminarPorId(Integer id);
 }
