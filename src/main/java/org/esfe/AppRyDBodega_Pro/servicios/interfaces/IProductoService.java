6632167b0f9df03cebc8bc77fb403aa761afeaf8
DANIEL-STEVEN-L0PEZ-HERNANDEZ <lh25001@esfe.agape.edu.sv>
2025-08-14 11:45:19 -0600

    Se modifico la interfaz de servicio de Producto

--- src/main/java/org/esfe/AppRyDBodega_Pro/servicios/interfaces/IProductoService.java
+++ src/main/java/org/esfe/AppRyDBodega_Pro/servicios/interfaces/IProductoService.java
@@ -1,11 +1,8 @@
 package org.esfe.AppRyDBodega_Pro.servicios.interfaces;
 
-import org.esfe.AppRyDBodega_Pro.modelos.Categoria;
 import org.esfe.AppRyDBodega_Pro.modelos.Producto;
-import org.esfe.AppRyDBodega_Pro.modelos.Proveedor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 
 import java.util.*;
 
@@ -16,5 +13,5 @@ public interface IProductoService {
     Optional<Producto>buscarPorId(Integer id);
     Producto createOrEditOne(Producto producto);
     void eliminarPorId (Integer id);
-    Page<Producto> buscarPorNombreYCategoriaYProveedor(String nombre, String categoriaNombre, String proveedorNombre, Pageable pageable);
+    Page<Producto> findByNombreContainingIgnoreCaseAndCategoriaNombreContainingIgnoreCaseAndProveedorNombreContainingIgnoreCaseOrderByIdAsc(String nombre, String categoriaNombre, String proveedorNombre, Pageable pageable);
 }
