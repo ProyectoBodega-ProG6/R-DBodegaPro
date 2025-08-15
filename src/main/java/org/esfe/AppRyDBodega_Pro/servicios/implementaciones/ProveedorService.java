f4aceebf9fca079a5203d28ea7788fe12552450a
ANDREA-ROCIO-PEREZ-ARRIOLA <pa25001@esfe.agape.edu.sv>
2025-08-15 10:06:20 -0600

    Se implementaron los metodos de la interfaz de proveedor

--- src/main/java/org/esfe/AppRyDBodega_Pro/servicios/implementaciones/ProveedorService.java
+++ src/main/java/org/esfe/AppRyDBodega_Pro/servicios/implementaciones/ProveedorService.java
@@ -1,4 +1,41 @@
 package org.esfe.AppRyDBodega_Pro.servicios.implementaciones;
 
-public class ProveedorService {
+import org.esfe.AppRyDBodega_Pro.modelos.Proveedor;
+import org.esfe.AppRyDBodega_Pro.servicios.interfaces.IProveedorService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+import java.util.Optional;
+
+public class ProveedorService implements IProveedorService {
+    @Override
+    public Page<Proveedor> buscarTodosPaginados(Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public List<Proveedor> obtenerTodos() {
+        return List.of();
+    }
+
+    @Override
+    public Optional<Proveedor> buscarPorId(Integer id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Proveedor createOrEditOne(Proveedor proveedor) {
+        return null;
+    }
+
+    @Override
+    public void eliminarPorId(Integer id) {
+
+    }
+
+    @Override
+    public Page<Proveedor> findByNombreContainingIgnoreCaseAndNombreEmpresaContainingIgnoreCaseOrderByIdAsc(String nombre, String nombreEmpresa, Pageable pageable) {
+        return null;
+    }
 }
