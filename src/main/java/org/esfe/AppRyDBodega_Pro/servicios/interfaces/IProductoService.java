6f425c43e7d888d4087852d8ec4ee0e4b729eda9
DANIEL-STEVEN-L0PEZ-HERNANDEZ <lh25001@esfe.agape.edu.sv>
2025-08-14 23:17:00 -0600

    Se modificó la interfaz de servicio de IProductoService, agregando las opciones de JpaRepository

--- src/main/java/org/esfe/AppRyDBodega_Pro/servicios/interfaces/IProductoService.java
+++ src/main/java/org/esfe/AppRyDBodega_Pro/servicios/interfaces/IProductoService.java
@@ -3,10 +3,11 @@ package org.esfe.AppRyDBodega_Pro.servicios.interfaces;
 import org.esfe.AppRyDBodega_Pro.modelos.Producto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.*;
 
-public interface IProductoService {
+public interface IProductoService extends JpaRepository<Producto, Integer> {
 
     Page<Producto> buscarTodosPaginados(Pageable pageable);
     List<Producto>obtenerTodos();
