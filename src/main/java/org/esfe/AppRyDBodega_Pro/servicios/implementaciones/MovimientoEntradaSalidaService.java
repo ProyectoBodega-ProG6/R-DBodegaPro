a90a0671982a07547ec2d8b9d6ddab21b81bb424
ANDREA-ROCIO-PEREZ-ARRIOLA <pa25001@esfe.agape.edu.sv>
2025-08-15 10:16:29 -0600

    Se modificaron los metodos de la implementacion MovimientoEntradaSalidaService

--- src/main/java/org/esfe/AppRyDBodega_Pro/servicios/implementaciones/MovimientoEntradaSalidaService.java
+++ src/main/java/org/esfe/AppRyDBodega_Pro/servicios/implementaciones/MovimientoEntradaSalidaService.java
@@ -1,7 +1,10 @@
 package org.esfe.AppRyDBodega_Pro.servicios.implementaciones;
 
 import org.esfe.AppRyDBodega_Pro.modelos.MovimientoEntradaSalida;
+import org.esfe.AppRyDBodega_Pro.repositorios.ICategoriaRepository;
+import org.esfe.AppRyDBodega_Pro.repositorios.IMovimientoEntradaSalidaRepository;
 import org.esfe.AppRyDBodega_Pro.servicios.interfaces.IMovimientoEntradaSalidaService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
@@ -10,38 +13,42 @@ import java.util.List;
 import java.util.Optional;
 
 public class MovimientoEntradaSalidaService implements IMovimientoEntradaSalidaService {
+
+    @Autowired
+    private IMovimientoEntradaSalidaRepository movimientoEntradaSalidaRepository;
+
     @Override
     public Page<MovimientoEntradaSalida> buscarTodosPaginados(Pageable pageable) {
-        return null;
+        return movimientoEntradaSalidaRepository.findAll(pageable);
     }
 
     @Override
     public List<MovimientoEntradaSalida> obtenerTodos() {
-        return List.of();
+        return movimientoEntradaSalidaRepository.findAll();
     }
 
     @Override
     public Optional<MovimientoEntradaSalida> buscarPorId(Integer id) {
-        return Optional.empty();
+        return movimientoEntradaSalidaRepository.findById(id);
     }
 
     @Override
     public MovimientoEntradaSalida createOrEditOne(MovimientoEntradaSalida movimientoEntradaSalida) {
-        return null;
+        return movimientoEntradaSalidaRepository.save(movimientoEntradaSalida);
     }
 
     @Override
     public void eliminarPorId(Integer id) {
-
+        movimientoEntradaSalidaRepository.deleteById(id);
     }
 
     @Override
     public Page<MovimientoEntradaSalida> findByProductoNombreContainingIgnoreCaseAndTipoMovimientoNombreOrderByIdAsc(String nombreProducto, String nombre, Pageable pageable) {
-        return null;
+        return movimientoEntradaSalidaRepository.findByProductoNombreContainingIgnoreCaseAndTipoMovimientoNombreOrderByIdAsc(nombreProducto, nombre, pageable);
     }
 
     @Override
     public List<MovimientoEntradaSalida> findByFechaBetween(LocalDateTime inicio, LocalDateTime fin) {
-        return List.of();
+        return movimientoEntradaSalidaRepository.findByFechaBetween(inicio, fin);
     }
 }
