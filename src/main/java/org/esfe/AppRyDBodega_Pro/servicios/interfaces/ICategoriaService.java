0d71cd28b2efc802d5964d84f6888da183ddc399
DANIEL-STEVEN-L0PEZ-HERNANDEZ <lh25001@esfe.agape.edu.sv>
2025-08-14 22:45:08 -0600

    Se modificó la interfaz de servicio de IRolService, agregando las opciones de JpaRepository

--- src/main/java/org/esfe/AppRyDBodega_Pro/servicios/interfaces/ICategoriaService.java
+++ src/main/java/org/esfe/AppRyDBodega_Pro/servicios/interfaces/ICategoriaService.java
@@ -3,11 +3,12 @@ package org.esfe.AppRyDBodega_Pro.servicios.interfaces;
 import org.esfe.AppRyDBodega_Pro.modelos.Categoria;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface ICategoriaService {
+public interface ICategoriaService extends JpaRepository<Categoria, Integer> {
 
     Page<Categoria> buscarTodosPaginados(Pageable pageable);
 
