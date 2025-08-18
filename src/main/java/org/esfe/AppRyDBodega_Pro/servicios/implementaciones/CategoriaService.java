08e79119db4c6886558db2bc39dbc2467640e48f
ANDREA-ROCIO-PEREZ-ARRIOLA <pa25001@esfe.agape.edu.sv>
2025-08-18 09:49:53 -0600

    Se añadio algo en la implementacion CategoriaService

--- src/main/java/org/esfe/AppRyDBodega_Pro/servicios/implementaciones/CategoriaService.java
+++ src/main/java/org/esfe/AppRyDBodega_Pro/servicios/implementaciones/CategoriaService.java
@@ -7,10 +7,12 @@ import org.esfe.AppRyDBodega_Pro.servicios.interfaces.ICategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class CategoriaService implements ICategoriaService {
 
     @Autowired
