e25ee45f66952fb15caf98e69ccd229d5e3f1374
DANIEL-STEVEN-L0PEZ-HERNANDEZ <lh25001@esfe.agape.edu.sv>
2025-08-24 18:24:12 -0600

    Modifique el archivo DatabaseWebSecurity, mainLayout y index, tambien cree el controlador LoginController y la vista Login

---
+++ src/main/java/org/esfe/AppRyDBodega_Pro/controladores/LoginController.java
@@ -0,0 +1,21 @@
+package org.esfe.AppRyDBodega_Pro.controladores;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+
+    @GetMapping
+    public String login(@RequestParam(value = "error", required = false) String error,
+                        Model model) {
+        if (error != null) {
+            model.addAttribute("errorMsg", "Usuario o contraseña incorrectos");
+        }
+        return "login";
+    }
+}
--- src/main/java/org/esfe/AppRyDBodega_Pro/security/DatabaseWebSecurity.java
+++ src/main/java/org/esfe/AppRyDBodega_Pro/security/DatabaseWebSecurity.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
@@ -18,10 +21,9 @@ public class DatabaseWebSecurity {
     @Bean
     public UserDetailsManager customUsers(DataSource dataSource){
         JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
-        users.setUsersByUsernameQuery("select username, password, status from usuarios where username = ?");
-        users.setAuthoritiesByUsernameQuery("select u.username, r.nombreRol from rol ur " +
-                "inner join usuarios u on r.id = u.usuario_rol " +
-                "inner join roles r on r.id = u.rol_rol " +
+        users.setUsersByUsernameQuery("select username, password, status as enabled from usuarios where username = ?");
+        users.setAuthoritiesByUsernameQuery("select u.username, r.nombreRol from usuarios u " +
+                "inner join roles r on r.id = u.id_rol " +
                 "where u.username = ?");
         return users;
     }
@@ -34,19 +36,33 @@ public class DatabaseWebSecurity {
                 .requestMatchers("/assets/**", "/css/**", "/js/**").permitAll()
 
                 //las vistas publicas que no requieren autenticación
-                .requestMatchers("/", "/privacy", "/terms").permitAll()
+                .requestMatchers("/privacy", "/terms", "/login").permitAll()
 
                 //todas las demas vistas necesitan autenticación
-                .requestMatchers("/grupos/**").hasAnyAuthority("admin")
-                .requestMatchers("/docentes/**").hasAnyAuthority("admin")
-                .requestMatchers("/asignaciones/**").hasAnyAuthority("admin")
-                .requestMatchers("/alumnos/**").hasAnyAuthority("admin", "docente")
+                .requestMatchers("/").hasAnyAuthority("Administrador", "SupervisorBodega")
+                .requestMatchers("/usuarios/**").hasAnyAuthority("Administrador")
+                .requestMatchers("/roles/**").hasAnyAuthority("Administrador")
+                .requestMatchers("/categorias/**").hasAnyAuthority("Administrador", "SupervisorBodega")
+                .requestMatchers("/proveedores/**").hasAnyAuthority("Administrador", "SupervisorBodega")
+                .requestMatchers("/productos/**").hasAnyAuthority("Administrador", "SupervisorBodega")
+                .requestMatchers("/tipoMovimientos/**").hasAnyAuthority("Administrador", "SupervisorBodega")
+                .requestMatchers("/movimientos/**").hasAnyAuthority("Administrador", "SupervisorBodega")
 
                 .anyRequest().authenticated());
-        http.formLogin(form -> form.loginPage("/login").permitAll());
+
+        http.formLogin(form -> form.loginPage("/login").permitAll().defaultSuccessUrl("/", true)
+                .failureUrl("/login?error=true") // Redirige a la página de login con un parámetro de error si falla
+        );
+
+        http.logout(logout -> logout
+                .logoutSuccessUrl("/login?logout") // Redirige a la página de login con un mensaje de "logout"
+                .permitAll());
 
         return http.build();
     }
 
-
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
 }
