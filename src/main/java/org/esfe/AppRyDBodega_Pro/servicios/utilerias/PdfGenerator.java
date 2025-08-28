a676e9914a9e703338b4deef4f049030a60cb847
DANIEL-STEVEN-L0PEZ-HERNANDEZ <lh25001@esfe.agape.edu.sv>
2025-08-28 03:50:33 -0600

    Se le dio estilo al documento para que se descargara de una mejor forma

--- src/main/java/org/esfe/AppRyDBodega_Pro/servicios/utilerias/PdfGenerator.java
+++ src/main/java/org/esfe/AppRyDBodega_Pro/servicios/utilerias/PdfGenerator.java
@@ -2,7 +2,6 @@ package org.esfe.AppRyDBodega_Pro.servicios.utilerias;
 
 import com.itextpdf.io.font.constants.StandardFonts;
 import com.itextpdf.io.image.ImageDataFactory;
-import com.itextpdf.kernel.colors.Color;
 import com.itextpdf.kernel.colors.ColorConstants;
 import com.itextpdf.kernel.font.PdfFontFactory;
 import com.itextpdf.kernel.pdf.PdfDocument;
@@ -13,10 +12,11 @@ import com.itextpdf.layout.element.Image;
 import com.itextpdf.layout.element.Paragraph;
 import com.itextpdf.layout.element.Table;
 import com.itextpdf.layout.properties.TextAlignment;
-import com.itextpdf.layout.properties.TextAlignment;
+import com.itextpdf.layout.properties.UnitValue;
 import org.esfe.AppRyDBodega_Pro.dto.KardexDTO;
 
 import java.io.OutputStream;
+import java.util.Arrays;
 import java.util.List;
 
 public class PdfGenerator {
@@ -25,6 +25,10 @@ public class PdfGenerator {
         try {
             PdfWriter writer = new PdfWriter(outputStream);
             PdfDocument pdfDoc = new PdfDocument(writer);
+
+            // ⬅ Cambiar orientación a horizontal
+            pdfDoc.setDefaultPageSize(com.itextpdf.kernel.geom.PageSize.A4.rotate());
+
             Document document = new Document(pdfDoc);
 
             // LOGO (ajusta la ruta a tu imagen en resources/static/img/logo.png)
@@ -46,9 +50,12 @@ public class PdfGenerator {
                     .setMarginBottom(20f)
             );
 
-            // Tabla con 14 columnas
-            float[] columnWidths = {100, 50, 50, 50, 40, 40, 60, 60, 50, 60, 40, 60, 60, 100};
+            // Tabla con 14 columnas iguales y ancho 100%
+            float[] columnWidths = new float[14];
+            Arrays.fill(columnWidths, 1);
             Table table = new Table(columnWidths);
+            table.setWidth(UnitValue.createPercentValue(100));
+            table.setKeepTogether(false); // permite dividir en páginas si es necesario
 
             String[] headers = {
                     "Producto", "Precio Compra", "Precio Venta", "Costo Promedio",
@@ -57,37 +64,37 @@ public class PdfGenerator {
                     "Fecha", "Observaciones"
             };
 
-            // Cabeceras con fondo gris
+            // Cabeceras con fondo gris y fuente más pequeña
             for (String header : headers) {
                 Cell cell = new Cell().add(new Paragraph(header)
                         .setFont(PdfFontFactory.createFont(StandardFonts.HELVETICA_BOLD))
-                        .setFontSize(10f)
+                        .setFontSize(8f)
                 );
                 cell.setBackgroundColor(ColorConstants.LIGHT_GRAY);
                 cell.setTextAlignment(TextAlignment.CENTER);
                 table.addHeaderCell(cell);
             }
 
-            // Filas con color alterno
+            // Filas con color alterno y fuente más pequeña
             boolean gris = false;
             for (KardexDTO k : listaKardex) {
                 gris = !gris;
-                Color rowColor = gris ? ColorConstants.WHITE : ColorConstants.LIGHT_GRAY;
+                com.itextpdf.kernel.colors.Color rowColor = gris ? ColorConstants.WHITE : ColorConstants.LIGHT_GRAY;
 
-                table.addCell(new Cell().add(new Paragraph(k.getNombreProducto())).setBackgroundColor(rowColor));
-                table.addCell(new Cell().add(new Paragraph(k.getPrecioCompra().toString())).setBackgroundColor(rowColor));
-                table.addCell(new Cell().add(new Paragraph(k.getPrecioVenta().toString())).setBackgroundColor(rowColor));
-                table.addCell(new Cell().add(new Paragraph(k.getCostoPromedio().toString())).setBackgroundColor(rowColor));
-                table.addCell(new Cell().add(new Paragraph(String.valueOf(k.getStockActual()))).setBackgroundColor(rowColor));
-                table.addCell(new Cell().add(new Paragraph(String.valueOf(k.getStockMinimo()))).setBackgroundColor(rowColor));
-                table.addCell(new Cell().add(new Paragraph(k.getCategoria())).setBackgroundColor(rowColor));
-                table.addCell(new Cell().add(new Paragraph(k.getProveedor())).setBackgroundColor(rowColor));
-                table.addCell(new Cell().add(new Paragraph(k.getEstadoStock())).setBackgroundColor(rowColor));
-                table.addCell(new Cell().add(new Paragraph(k.getTipoMovimiento())).setBackgroundColor(rowColor));
-                table.addCell(new Cell().add(new Paragraph(String.valueOf(k.getCantidad()))).setBackgroundColor(rowColor));
-                table.addCell(new Cell().add(new Paragraph(k.getPrecioMovimiento().toString())).setBackgroundColor(rowColor));
-                table.addCell(new Cell().add(new Paragraph(k.getFecha().toString())).setBackgroundColor(rowColor));
-                table.addCell(new Cell().add(new Paragraph(k.getObservaciones() != null ? k.getObservaciones() : "")).setBackgroundColor(rowColor));
+                table.addCell(new Cell().add(new Paragraph(k.getNombreProducto()).setFontSize(8f)).setBackgroundColor(rowColor));
+                table.addCell(new Cell().add(new Paragraph(k.getPrecioCompra().toString()).setFontSize(8f)).setBackgroundColor(rowColor));
+                table.addCell(new Cell().add(new Paragraph(k.getPrecioVenta().toString()).setFontSize(8f)).setBackgroundColor(rowColor));
+                table.addCell(new Cell().add(new Paragraph(k.getCostoPromedio().toString()).setFontSize(8f)).setBackgroundColor(rowColor));
+                table.addCell(new Cell().add(new Paragraph(String.valueOf(k.getStockActual())).setFontSize(8f)).setBackgroundColor(rowColor));
+                table.addCell(new Cell().add(new Paragraph(String.valueOf(k.getStockMinimo())).setFontSize(8f)).setBackgroundColor(rowColor));
+                table.addCell(new Cell().add(new Paragraph(k.getCategoria()).setFontSize(8f)).setBackgroundColor(rowColor));
+                table.addCell(new Cell().add(new Paragraph(k.getProveedor()).setFontSize(8f)).setBackgroundColor(rowColor));
+                table.addCell(new Cell().add(new Paragraph(k.getEstadoStock()).setFontSize(8f)).setBackgroundColor(rowColor));
+                table.addCell(new Cell().add(new Paragraph(k.getTipoMovimiento()).setFontSize(8f)).setBackgroundColor(rowColor));
+                table.addCell(new Cell().add(new Paragraph(String.valueOf(k.getCantidad())).setFontSize(8f)).setBackgroundColor(rowColor));
+                table.addCell(new Cell().add(new Paragraph(k.getPrecioMovimiento().toString()).setFontSize(8f)).setBackgroundColor(rowColor));
+                table.addCell(new Cell().add(new Paragraph(k.getFecha().toString()).setFontSize(8f)).setBackgroundColor(rowColor));
+                table.addCell(new Cell().add(new Paragraph(k.getObservaciones() != null ? k.getObservaciones() : "").setFontSize(8f)).setBackgroundColor(rowColor));
             }
 
             document.add(table);
